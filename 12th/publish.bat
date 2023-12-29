c92419b59349bfa5a0816d9b323cbb7f7212057e
faizan_bdnews24 <faizan.siddiqui@bdnews24.com>
2023-12-29 16:52:45 +0600

    $msg

--- 12th/publish.bat
+++ 12th/publish.bat
@@ -2,7 +2,9 @@ echo %DATE% %TIME% >> ../../logs/tally/backup.log
 echo adding
 git add .
 echo commit
-git commit -m "updated at $(Get-Date)"
+current="`date +'%Y-%m-%d %H:%M:%S'`"
+msg="Updated: $current"
+git commit -m "$msg"
 echo push
 git push origin main
 pause
